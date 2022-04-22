a2a5a79a1cf21f8a232d3a5f0e480c60dc0493a0
tomthomas1 <tomthomas8688@gmail.com>
2022-04-22 13:48:27 +0530

    Refactored to calculate minimum fare and total fare

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -15,4 +15,15 @@ public class InvoiceServiceTest {
        double fare = invoiceGenerator.calculateFare(distance, time);
        assertEquals(25, fare, 0.0);
     }
+
+    //UC 1: Minimum fare
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        assertEquals(5, fare, 0.0);
+    }
+
 }
